e0facf2af56786c065550c959366e83aea27a612
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-07-27 21:48:55 -0700

    remove useless comments

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,74 +1,48 @@
 class LineItemsController < ApplicationController
-  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_line_item, only: %i[show edit update destroy]
 
-  # GET /line_items
-  # GET /line_items.json
   def index
     @line_items = LineItem.all
   end
 
-  # GET /line_items/1
-  # GET /line_items/1.json
-  def show
-  end
+  def show; end
 
-  # GET /line_items/new
   def new
     @line_item = LineItem.new
   end
 
-  # GET /line_items/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /line_items
-  # POST /line_items.json
   def create
     @line_item = LineItem.new(line_item_params)
 
-    respond_to do |format|
-      if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.json { render :show, status: :created, location: @line_item }
-      else
-        format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
+    if @line_item.save
+      redirect_to @line_item.order, notice: 'Line item was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /line_items/1
-  # PATCH/PUT /line_items/1.json
   def update
-    respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
+    if @line_item.update(line_item_params)
+      redirect_to @line_item.order, notice: 'Line item was successfully updated.'
+    else
+      redirect_to @line_item.order, notice: 'Line item failed to updated.'
     end
   end
 
-  # DELETE /line_items/1
-  # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
-    respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @line_item.destroy!
+    redirect_to @line_item.order, notice: 'Line item was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def line_item_params
-      params.require(:line_item).permit(:seller, :product, :order_id, :quantity)
-    end
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
+
+  def line_item_params
+    params.require(:line_item).permit(:seller, :product, :order_id, :quantity)
+  end
 end
